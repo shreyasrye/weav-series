c44b444e7ec90657285574e26323fee36e38f493
shreyasrye <your_email@example.com>
2022-12-29 10:08:42 -0800

    updated the find_date_column() function, but does not work as intended

--- time-series.py
+++ time-series.py
@@ -32,21 +32,43 @@ def any_valid(dframe):
     # return intersection of booleans
     return hasDate and hasNum
 
-# Assuming a dataframe is roughly valid, find_date_column() checks if there exists a column in dframe that 
-# contains a series of parsable dates which have over 90% non-null values and a chronological series of dates.
-def find_date_column(dframe):
-    return None
+# Assuming a dataframe is roughly valid, find_date_column() 
+# checks if there exists 1 or more columns in dframe that 
+# contains a series of parsable dates which have over 90% non-null 
+# values and a chronological series of dates.
+# returns: a list of all date columns in dframe, and an empty list if none exist
+def find_date_columns(dframe): # INCOMPLETE
+    # store date columns in this list
+    d_ls = []
+
+    # A column that has date values stored as datetime64[ns] makes our problem  easy
+    for col in dframe.columns:
+        if dframe[col].dtype == 'datetime64[ns]': 
+            percent_datetime = dframe[col].notnull().mean()
+            if percent_datetime >= 0.9 and (dframe[col].is_monotonic_increasing or dframe[col].is_monotonic_decreasing):
+                d_ls.append(col)
+                continue
+        column_values = pd.to_datetime(dframe[col], errors='coerce')
+        percent_datetime = column_values.notnull().mean()
+        print("percentage: " + str(percent_datetime))
+        if(col == 'Date'):
+            print("YES")
+        if percent_datetime >= 0.9 and (dframe[col].is_monotonic_increasing or dframe[col].is_monotonic_decreasing):
+            d_ls.append(col)
+            continue
+    return d_ls
 
 def groupby(dframe, group_factor): # group by date, month, year, week
     # Current Pseudocode:
         # - check rough validity first
         # - if roughly valid, find a date column (and simultaneously determine if fully valid)
-        # - if a column is found, preprocess data (if needed) and then create a time series by grouping
+        # - if a column i s found, preprocess data (if needed) and then create a time series by grouping
     return None
 
 
 # testing
 
-df = pd.read_csv('datasets/christmas_movies.csv')
+df = pd.read_csv('datasets/1979-2021.csv')
+# print(df.dtypes)
 print(any_valid(df))
 
