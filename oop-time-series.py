abb56556ef246a03cbe319356829f2a3a927e32d
shreyasrye <shreyasrai23@gmail.com>
2023-01-28 15:32:15 -0800

    added decomposition and new display method, still needs work

--- oop-time-series.py
+++ oop-time-series.py
@@ -2,30 +2,35 @@ import pandas as pd
 import datetime
 import numpy as np
 import math
+from collections import OrderedDict
+from statsmodels.tsa.seasonal import seasonal_decompose
+import matplotlib.pyplot as plt
 
 
 class TimeSeries():
     
-    # Fields
-    dframe, time_series, frequency, agg_operations, date_column = None, None, None, None, None
+    # Declaration and initialization of attributes
+    dframe, time_series, frequency, agg_operations, date_column, date_column_frequency, decomposition = None, None, None, None, None, None, None
     value_columns = []
-    is_irregular = False
+    is_irregular, is_time_series = False, False
     interpolation_method = 'linear'
     validity_threshold = 0.4
-    frequency_multipliers = { 
-        'N': 1/1000000000,
-        'U': 1/1000000,
-        'L': 1/1000,
-        'S': 1,
-        'T': 60,# minute
-        'H': 3600, # hour
-        'D': 86400, # day
-        'B': 86400,
-        'W': 7*86400, # week
-        'M': 30.5*7*86400, # month
-        'Q': 3*30.5*7*86400,
-        'Y': 365.25*86400  # year
-    }
+
+    frequency_multipliers = OrderedDict() # stores the intervals between units of each frequency in terms of seconds
+    frequency_multipliers['N'] = 1/1000000000
+    frequency_multipliers['U'] = 1/1000000
+    frequency_multipliers['L'] = 1/1000
+    frequency_multipliers['S'] = 1
+    frequency_multipliers['T'] = 60# minute
+    frequency_multipliers['H'] = 3600 # hour
+    frequency_multipliers['D'] = 86400, # day
+    frequency_multipliers['B'] = 86400,
+    frequency_multipliers['W'] = 7*86400, # week
+    frequency_multipliers['M'] = 30.5*7*86400, # month
+    frequency_multipliers['Q'] = 3*30.5*7*86400,
+    frequency_multipliers['Y'] = 365.25*86400  # year
+    
+
     possible_operations = ['sum', 'mean', 'min', 'max', 'std', 'first', 'last', \
     'median', 'var', 'sem', 'skew', 'quantile']
 
@@ -36,7 +41,7 @@ class TimeSeries():
         # Preliminary checks for the provided desired frequency and operation(s)
         if frequency:
             self.frequency = frequency
-        f_num = frequency[0]
+        f_num = frequency[0] 
         try: 
             f_num = int(f_num)
         except ValueError:
@@ -56,27 +61,27 @@ class TimeSeries():
             self.agg_operations = self.possible_operations
 
         # categorize (and format) the columns of importance
-        cats = self.categorize_columns(self.dframe)
+        categories = self.categorize_columns(self.dframe)
 
         # checks whether the input dataframe contains at least 1 numerical column and 
         # 1 date column where the dates are either a continuous series or irregular  
-        if not self.is_compatible(self.dframe, cats):
-            print("A time series cannot be created from this data")
+        if not self.is_compatible(self.dframe, categories):
+            print("A time series cannot be created from this data") # TODO specify
             return
         
         # If not specified, find the date column with the highest frequency
         if date_column:
             # Check to see if the provided column exists in the dataframe
-            if date_column not in cats['d']:
+            if date_column not in categories['d']:
                 print("Invalid Date column provided")
                 return
             self.date_column = date_column   
         else:    
-            modes = self.find_date_intervals(self.dframe, cats['d'])
+            modes = self.find_date_intervals(self.dframe, categories['d']) # TODO could do this in is_compatible?
             self.date_column = min(modes, key=modes.get)
 
         # check if the frequency provided is lower than the frequency of the date column 
-        if not self.compare_frequencies(self.dframe, self.frequency, self.date_column):
+        if not self.compare_frequencies(self.dframe, self.frequency, self.date_column): # TODO if not provided, set frequency to natural freq
             print("This frequency is not applicable for this dataframe: pick a lower frequency")
             return
 
@@ -88,10 +93,9 @@ class TimeSeries():
             self.value_columns = value_columns
         # otherwise, include all numerical columns
         else:
-            self.value_columns = cats['v']
+            self.value_columns = categories['v']
 
-        # Resample the dataframe once preliminary checks are over
-
+    # Resample the dataframe once preliminary checks are over
         # Generate the time series
         self.dframe.set_index(self.date_column, inplace=True)
 
@@ -103,10 +107,27 @@ class TimeSeries():
         for vc in self.value_columns:
             self.time_series[vc] = self.time_series[vc].interpolate(method=self.interpolation_method)
 
+        plt.plot(self.time_series)
+
+        # perform the decomposition for analytics
+        self.decomposition = seasonal_decompose(self.time_series[self.value_columns], model='multiplicable')
+
 
     # Displays the generated time series
     def display(self): 
-        return self.time_series
+        # print out the dataframe of the time series
+        print(self.time_series)
+
+        # Print out the information about the time series
+        print()
+        print(f"Frequency: {self.frequency}")
+        print(f"Value Columns: {self.value_columns}")
+        print(f"Trend: {self.decomposition.trend}")
+        print(f"Seasonality: {self.decomposition.seasonal}")
+        print(f"Residual: {self.decomposition.resid}")
+
+        plt.show()
+
 
 
     # Given date columns, return a dictionary of the modes of the intervals of each one
@@ -132,8 +153,8 @@ class TimeSeries():
             desired_frequency_offset = self.frequency_multipliers[desired_freq[1:]]*f_num
 
         # Find the most common difference between dates in date_column in seconds
-        date_col_modes = self.find_date_intervals(dframe,[date_column])
-        date_freq_offset = date_col_modes[date_column].total_seconds()
+        date_col_mode = self.find_date_intervals(dframe,[date_column])
+        date_freq_offset = date_col_mode[date_column].total_seconds()
 
         # Now, both values are converted to seconds, so they can be compared
         return desired_frequency_offset >= date_freq_offset
@@ -152,22 +173,27 @@ class TimeSeries():
             diffs = dframe[d].diff()
             interval = diffs.value_counts()
             # If there exist more than 7 different intervals, say that the dates aren't applicable
-            if len(interval) > 0 and len(interval) < 7:
+            if len(interval) > 0 and len(interval) < 7: # TODO check if arrow has a way to do this
                 hasContinuousDateColumn = True
                 break
         # check for irregular data (for example, timestamp data) 
             else:
-                # self.is_irregular = True
-                hasContinuousDateColumn = True
+                self.is_irregular = True
         # check for numerical values
         if any(dframe.dtypes == 'int64') or any(dframe.dtypes == 'float64'):
             hasNum = True
 
-        return hasContinuousDateColumn and hasNum
+        return (hasContinuousDateColumn and hasNum) or (self.is_irregular and hasNum)
+
+
+    # If the dates are irregular, then we downsample them until they become regular
+    def down_sample_to_regular(self, dframe, date_col, freq_dict):
+        average_interval = dframe[date_col].diff().dt.total_seconds().mean()
+        return 
 
 
     # formats the provided column to datetime64
-    def format_column(self, dframe, date_col):
+    def format_column(self, dframe, date_col): # change name
         # if date_col is already formatted correctly, then we don't need to do anything
         if dframe[date_col].dtype == 'datetime64[ns]':
             return dframe[date_col]
@@ -225,6 +251,6 @@ class TimeSeries():
 
     
 
-df = pd.read_csv('datasets/pr_transactions.csv')
-ts = TimeSeries(df, '3D', 'mean')
-print(ts.display())
+df = pd.read_csv('datasets/1979-2021.csv')
+ts = TimeSeries(df, '3Y', 'mean', ['United States(USD)'])
+ts.display()
